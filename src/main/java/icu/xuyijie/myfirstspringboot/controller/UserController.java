1d46c18478cb01810b04fe2d50ecf8441b52decf
XuYijie <1119461672@qq.com>
2024-10-31 16:00:47 +0800

    增加日志打印

--- src/main/java/icu/xuyijie/myfirstspringboot/controller/UserController.java
+++ src/main/java/icu/xuyijie/myfirstspringboot/controller/UserController.java
@@ -1,5 +1,8 @@
 package icu.xuyijie.myfirstspringboot.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,16 +14,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @RequestMapping("/user")
 @Controller
+@Slf4j
 public class UserController {
+    //private final Logger log = LoggerFactory.getLogger(UserController.class)
+
     @RequestMapping("/login")
     public String login(String username, String password, Model model) {
-        System.out.println(username);
-        System.out.println(password);
+        log.info("前端输入的用户名是：{},前端输入的密码是：{}", username, password);
 
         if ("1".equals(username) && "1".equals(password)) {
-            System.out.println("登录成功");
+            log.info("登录成功");
         } else {
-            System.out.println("登录失败");
+            log.error("登录失败");
             model.addAttribute("msg", "登录失败");
         }
 
