53640708f69fd31e51a23364a410e319cb735afa
XuYijie <1119461672@qq.com>
2024-11-14 11:38:17 +0800

    学生查询动态SQL

--- src/main/java/icu/xuyijie/myfirstspringboot/mapper/StudentMapper.java
+++ src/main/java/icu/xuyijie/myfirstspringboot/mapper/StudentMapper.java
@@ -18,7 +18,7 @@ public interface StudentMapper {
      * 查询学生列表
      * @return 学生列表
      */
-    @Select("SELECT s.id,s.name,s.age,s.sex,s.teacher,s.img_url,s.stu_id,s.create_time,s.is_graduate,s.class AS className,t.name AS teacherName FROM `student` s LEFT JOIN teacher t ON t.id = s.teacher WHERE s.name = #{name} AND s.class = #{className};")
+    //@Select("SELECT s.id,s.name,s.age,s.sex,s.teacher,s.img_url,s.stu_id,s.create_time,s.is_graduate,s.class AS className,t.name AS teacherName FROM `student` s LEFT JOIN teacher t ON t.id = s.teacher WHERE s.name = #{name} AND s.class = #{className};")
     List<Student> getStuentList(String name, String className);
 
     /**
